349b401bd4a41d58e472590afb0f3053e7dfcddc
Manvir <sehmbimanvir@gmail.com>
2020-01-27 23:51:21 +0530

    Working on Login Page

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import Boxed from '../shared/Boxed'
+import { Row, Col, Form, Input, Icon, Typography, Button } from 'antd'
 
-const Login = () => (
-  <Boxed>
-    <h1>Login Component</h1>
-  </Boxed>
-)
+const { Title } = Typography
 
-export default Login
+const Login = ({ form }) => {
+
+  const { getFieldDecorator } = form
+
+  const handleOnSubmit = e => {
+    e.preventDefault()
+    form.validateFields((err, values) => {
+      if (!err) {
+        console.log(values)
+      }
+    })
+  }
+  return (
+    <Boxed>
+      <Row className='login-form'>
+        <Col span={6} offset={9}>
+          <Title level={3}>Login to continue...</Title>
+          <Form onSubmit={handleOnSubmit}>
+            <Form.Item>
+              {getFieldDecorator('email', {
+                rules: [{ required: true, message: 'Email is required' }, { email: true, message: 'Invalid Email' }]
+              })(
+                <Input prefix={<Icon type='user' />} placeholder='Email' type='email' />
+              )}
+            </Form.Item>
+
+            <Form.Item>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Password is required' }]
+              })(
+                <Input prefix={<Icon type='lock' />} placeholder='Password' type='password' />
+              )}
+            </Form.Item>
+
+            <Form.Item>
+              <Button htmlType='submit' type='primary'>Login</Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    </Boxed>
+  )
+}
+
+export default Form.create({ name: 'login-form' })(Login)
---
+++ src/store/reducers/auth.reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  token: null,
+  expired_at: null,
+  user: {}
+}
+
+const reducer = (state = initialState, action) => {
+  return state
+}
+
+export default reducer
